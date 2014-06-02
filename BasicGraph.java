6ba169dba524ffc294239ba20c2a4c76bd6ce316
PlasmaPong <codymichael92@yahoo.com>
2014-06-02 02:45:09 -0700

    sdfa
    asdfasf

--- BasicGraph.java
+++ BasicGraph.java
@@ -1,5 +1,14 @@
 package Graph;
 
-public interface BasicGraph {
-
+public interface BasicGraph<E, K> {
+	public void addVertex(Vertex vertex);
+	
+	public void addEdge(int start, int end, int weight);
+	
+	public E getVertex(E entry);
+	
+	public ArrayList getVertices();
+	
+	public E getEdge
+	
 }
--- Graph.java
+++ Graph.java
@@ -1,5 +1,23 @@
 package Graph;
 
-public class Graph {
+import java.util.ArrayList;
 
+public class Graph<E, V> {
+	/**
+	 * Connected, directional, cyclic 
+	 * 2d adjacency array for edges
+	 */
+	ArrayList vertices = new ArrayList();
+	ArrayList<ArrayList> edges;
+	public Graph(){
+		edges =	new ArrayList<ArrayList>();
+	}
+	public void addVertex(V value){
+		
+	}
+	private class Vertex{
+		private Vertex(){
+			
+		}
+	}
 }
